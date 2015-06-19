338727aa2f522d6b887aed199bd511e5218c1a61
Stuart Alldritt <stuart.alldritt@rogers.com>
2015-06-18 22:34:02 -0400

    Adding initial Keil project for Sunflower

---
+++ devkit/Objects/system_interrupts.d
@@ -0,0 +1,10 @@
+.\objects\system_interrupts.o: bootloader\src\system_interrupts.c
+.\objects\system_interrupts.o: .\bootloader\inc\system_interrupts.h
+.\objects\system_interrupts.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.4.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f4xx.h
+.\objects\system_interrupts.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.4.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f407xx.h
+.\objects\system_interrupts.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.3.0\CMSIS\Include\core_cm4.h
+.\objects\system_interrupts.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\system_interrupts.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.3.0\CMSIS\Include\core_cmInstr.h
+.\objects\system_interrupts.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.3.0\CMSIS\Include\core_cmFunc.h
+.\objects\system_interrupts.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.3.0\CMSIS\Include\core_cmSimd.h
+.\objects\system_interrupts.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.4.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
---
+++ devkit/app/inc/led_task.h
@@ -0,0 +1,7 @@
+#ifndef __LED_TASK_H
+#define __LED_TASK_H
+
+void LedBlinkTask(const void *argument);
+void Led2BlinkTask(const void *argument);
+
+#endif // __LED_TASK_H
